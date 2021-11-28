f43fba3c709b010d771d1c8a7a6344af444a1578
Alessio <galatolo@kth.se>
2021-11-28 15:37:22 +0100

    Add losses skeleton

---
+++ GAN2Shape/losses.py
@@ -0,0 +1,12 @@
+from GAN2Shape.stylegan2 import PerceptualLoss
+
+
+# TODO: decide on whether to have a class for the losses or a function
+
+
+class DiscriminatorLoss():
+    ...
+
+
+class PhotometricLoss():
+    ...
