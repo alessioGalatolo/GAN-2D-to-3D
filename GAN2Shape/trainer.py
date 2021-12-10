2c8ba99bc66e53b6b791276c1e012a8ae9ff1ce0
Alessio <galatolo@kth.se>
2021-12-10 10:19:28 +0100

    Add more categories for masking, code style changes

--- GAN2Shape/trainer.py
+++ GAN2Shape/trainer.py
@@ -1,4 +1,3 @@
-from logging import debug
 import math
 import torch
 from tqdm import tqdm
@@ -10,6 +9,12 @@ from gan2shape import utils
 
 
 class Trainer():
+    CATEGORIES = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+                  'car', 'cat', 'chair', 'cow', 'diningtable', 'dog',
+                  'horse', 'motorbike', 'person', 'pottedplant',
+                  'sheep', 'sofa', 'train', 'tvmonitor']
+    CATEGORY2NUMBER = {category: i for i, category in enumerate(CATEGORIES)}
+
     def __init__(self,
                  model,
                  model_config,
@@ -19,22 +24,26 @@ class Trainer():
                  save_ckpts=False):
         self.model = model(model_config, debug)
         self.image_size = model_config.get('image_size')
+        self.category = model_config.get('category')
         self.n_epochs_prior = model_config.get('n_epochs_prior', 1000)
         self.learning_rate = model_config.get('learning_rate', 1e-4)
         self.plot_intermediate = plot_intermediate
         self.log_wandb = log_wandb
         self.save_ckpts = save_ckpts
         self.debug = debug
-        self.prior_name =  model_config.get('prior_name', "box")
+        self.prior_name = model_config.get('prior_name', "box")
 
     def fit(self, images, latents, plot_depth_map=False):
         # self.model.reinitialize_model()
-        self.optim_step1 = Trainer.default_optimizer([self.model.albedo_net], lr=self.learning_rate)
-        self.optim_step2 = Trainer.default_optimizer([self.model.offset_encoder_net], lr=self.learning_rate)
-        self.optim_step3 = Trainer.default_optimizer([  self.model.lighting_net,
-                                                        self.model.viewpoint_net,
-                                                        self.model.depth_net,
-                                                        self.model.albedo_net], lr=self.learning_rate)
+        self.optim_step1 = Trainer.default_optimizer([self.model.albedo_net],
+                                                     lr=self.learning_rate)
+        self.optim_step2 = Trainer.default_optimizer([self.model.offset_encoder_net],
+                                                     lr=self.learning_rate)
+        self.optim_step3 = Trainer.default_optimizer([self.model.lighting_net,
+                                                      self.model.viewpoint_net,
+                                                      self.model.depth_net,
+                                                      self.model.albedo_net],
+                                                     lr=self.learning_rate)
 
         self.reconstructions = {'images': [None] * len(images), 'depths': [None] * len(images)}
         total_it = 0
@@ -54,7 +63,7 @@ class Trainer():
         shuffle_ids = [0]
         # Sequential training of the D,A,L,V nets
 
-        #-----------------Main loop through all images--------------------------
+        # -----------------Main loop through all images--------------------------
         iterator = tqdm(shuffle_ids)
         for i_batch in shuffle_ids:
             print(f'Training on image {i_batch}/{len(shuffle_ids)}')
@@ -64,28 +73,27 @@ class Trainer():
             # Pretrain depth net on the prior shape
             self.pretrain_on_prior(image_batch, i_batch, plot_depth_map)
 
-            #-----------------Loop through all stages---------------------------
+            # -----------------Loop through all stages---------------------------
             for stage in range(self.n_stages):
                 iterator.set_description("Stage: " + str(stage) + "/"
                                          + str(self.n_stages) + ". Image: "
                                          + str(i_batch+1) + "/"
                                          + str(len(images)) + ".")
-                running_loss = 0.0
                 old_collected = [None]*len(images)
 
-                #-----------------------Step 1 and 2----------------------------
+                # -----------------------Step 1 and 2----------------------------
                 for step in [1, 2]:
                     print(f"Doing step {step}, stage {stage + 1}/{self.n_stages}")
                     step_iterator = tqdm(range(stages[stage][f'step{step}']))
                     current_collected = [None]*len(images)
-                    optim = getattr(self,f'optim_step{step}')
+                    optim = getattr(self, f'optim_step{step}')
                     for _ in step_iterator:
                         optim.zero_grad()
                         collected = old_collected[i_batch]
 
                         loss, collected = getattr(self.model, f'forward_step{step}')\
                             (image_batch, latent_batch, collected)
-                        
+
                         current_collected[i_batch] = collected
                         loss.backward()
                         optim.step()
@@ -99,7 +107,7 @@ class Trainer():
                                        "image_num": i_batch})
                     old_collected = current_collected
 
-                #-----------------------------Step 3----------------------------
+                # -----------------------------Step 3----------------------------
                 print(f"Doing step 3, stage {stage + 1}/{self.n_stages}")
                 step_iterator = tqdm(range(stages[stage]['step3']))
                 current_collected = []
@@ -111,7 +119,8 @@ class Trainer():
                     shuffle(shuffle_projected)
                     for i_proj in shuffle_projected:
                         optim.zero_grad()
-                        collected = projected_samples[i_proj].unsqueeze(0).cuda(), masks[i_proj].unsqueeze(0).cuda()
+                        collected = projected_samples[i_proj].unsqueeze(0).cuda(),\
+                            masks[i_proj].unsqueeze(0).cuda()
 
                         loss, _ = self.model.forward_step3(image_batch, latent_batch, collected)
                         loss.backward()
@@ -130,15 +139,12 @@ class Trainer():
                         recon_im, recon_depth = self.model.evaluate_results(image_batch)
                         recon_im, recon_depth = recon_im.cpu(), recon_depth.cpu()
                         plot_reconstructions(recon_im, recon_depth,
-                                total_it=str(total_it), im_idx=str(i_batch), stage=str(stage))
-                
+                                             total_it=str(total_it),
+                                             im_idx=str(i_batch),
+                                             stage=str(stage))
+
                 if self.save_ckpts:
                     self.model.save_checkpoint(stage, total_it)
-
-
-            # print(f'Loss: {running_loss}') # FIXME
-            # maybe WandB logging is enough?
-
         print('Finished Training')
 
     def pretrain_on_prior(self, image, i_batch, plot_depth_map):
@@ -158,12 +164,12 @@ class Trainer():
             if epoch % self.n_epochs_prior / 10 == 0:
                 with torch.no_grad():
                     iterator.set_description("Epoch (prior): " + str(epoch+1) + "/"
-                                            + str(self.n_epochs_prior)
-                                            + ". Loss = " + str(loss.cpu()))
-                    
+                                             + str(self.n_epochs_prior)
+                                             + ". Loss = " + str(loss.cpu()))
+
             if self.log_wandb:
-                wandb.log({"loss_prior":loss.cpu(),
-                            "image_num": i_batch})
+                wandb.log({"loss_prior": loss.cpu(),
+                           "image_num": i_batch})
 
         if plot_depth_map:
             self.model.plot_predicted_depth_map(image)
@@ -184,16 +190,8 @@ class Trainer():
                 h, w = self.image_size, self.image_size
                 c_x, c_y = w / 2, h / 2
 
-                ## FIXME: utils.get_mask_range(mask) throws error
-                # see Trainer.image_mask() for more details
-
-                # mask = self.image_mask(image)[0, 0] >= 0.7
-                # max_y, min_y, max_x, min_x = utils.get_mask_range(mask)
-
-                #Temporary "fix"
-                box_height, box_width = int(h*0.5*0.5), int(w*0.8*0.5)
-                max_x, min_x = int(c_x+box_width), int(c_x-box_width)
-                max_y, min_y = int(c_y+box_height), int(c_y-box_height)
+                mask = self.image_mask(image)[0, 0] >= 0.7
+                max_y, min_y, max_x, min_x = utils.get_mask_range(mask)
 
                 # if self.category in ['car', 'church']:
                 #     max_y = max_y + (max_y - min_y) / 6
@@ -230,20 +228,21 @@ class Trainer():
             image[:, 2].sub_(0.406).div_(0.225)
             out = self.model.mask_net(image)
             out = out.argmax(dim=1, keepdim=True)
-            # FIXME: out has no elems == 7, I think something may
-            # be wrong with the mask_net 
-            # This results in an error in Trainer.prior_shape() for the ellipsoid
-            # because the mask is only False
-            # => which gives an error in utils.get_mask_range(mask)
-            mask = (out == 7).float()  # FIXME: only cars
-        return utils.resize(mask, [self.image_size, self.image_size])
+            if self.category in Trainer.CATEGORIES:
+                mask = (out == Trainer.CATEGORY2NUMBER[self.category])
+            else:
+                mask = torch.ones(out.size(), dtype=torch.bool)
+
+            if not torch.any(mask):
+                mask = torch.ones(out.size(), dtype=torch.bool)
+        return utils.resize(mask.float(), [self.image_size, self.image_size])
 
     @staticmethod
     def default_optimizer(model_list, lr=1e-4, betas=(0.9, 0.999), weight_decay=5e-4):
         param_list = []
         for model in model_list:
             params = filter(lambda param: param.requires_grad,
-                                  model.parameters())
-            param_list+=list(params)
+                            model.parameters())
+            param_list += list(params)
         return torch.optim.Adam(param_list, lr=lr,
                                 betas=betas, weight_decay=weight_decay)
