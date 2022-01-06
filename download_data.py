aba5c899f98319f882e7bb1509977e927cf073b2
Alessio <galatolo@kth.se>
2022-01-06 16:59:20 +0100

    Fix data downloader

--- download_data.py
+++ download_data.py
@@ -4,32 +4,40 @@ import urllib.request
 from glob import glob
 import os
 
-print("Downloading data...")
+print("Downloading data...", flush=True)
 urllib.request.urlretrieve('https://github.com/XingangPan/GAN2Shape/releases/download/v1.0/data.tar.gz', "data.tar.gz")
+parts = [f'x0{i}' for i in range(4)]
 urllib.request.urlretrieve('https://github.com/XingangPan/GAN2Shape/releases/download/v1.0/x00', 'x00')
 urllib.request.urlretrieve('https://github.com/XingangPan/GAN2Shape/releases/download/v1.0/x01', 'x01')
 urllib.request.urlretrieve('https://github.com/XingangPan/GAN2Shape/releases/download/v1.0/x02', 'x02')
 urllib.request.urlretrieve('https://github.com/XingangPan/GAN2Shape/releases/download/v1.0/x03', 'x03')
 
-print("Data downloaded, saving it...")
-shutil.copyfile('x00', 'x0.tar.gz')
-shutil.copyfile('x01', 'x0.tar.gz')
-shutil.copyfile('x02', 'x0.tar.gz')
-shutil.copyfile('x03', 'x0.tar.gz')
-print("Extracting data...")
+print("Data downloaded, saving it...", flush=True)
+with open('x0.tar.gz', 'wb') as dest:
+    for file in parts:
+        with open(file, 'rb') as src:
+            shutil.copyfileobj(src, dest)
+
+print("Extracting data...", flush=True)
 with tarfile.open('data.tar.gz', mode="r:gz") as data_tar:
     data_tar.extractall()
 with tarfile.open('x0.tar.gz', mode="r:gz") as checkpoints_tar:
     checkpoints_tar.extractall()
 
+print("Removing temp files...")
 os.remove('data.tar.gz')
 os.remove('x0.tar.gz')
-os.remove('x00')
-os.remove('x01')
-os.remove('x02')
-os.remove('x03')
-for category in os.walk("data"):
-    filelists = glob(f'data/{category}/list*')
-    if len(filelists) > 1:
-        for filelist in filelists:
-            shutil.copyfile(filelist, f"data/{category}/list.txt")
+for part in parts:
+    os.remove(part)
+
+for _, dirs, _ in os.walk("data"):
+    for category in dirs:
+        filelists = glob(f'data/{category}/list*')
+        if len(filelists) > 1:
+            with open(f"data/{category}/list.txt", 'wb') as dest:
+                for filelist in filelists:
+                    try:
+                        with open(filelist, 'rb') as src:
+                            shutil.copyfileobj(src, dest)
+                    except shutil.SameFileError:
+                        pass
